6f985c2ba6ca3d0d554fec122b2ac4012cce7a29
Alexander Konukhov <jesusmisha@gmail.com>
2023-02-22 23:48:49 +0300

    all good

--- config/qase.py
+++ config/qase.py
@@ -1,10 +1,15 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Qase:
     TOKEN = os.getenv('QASE_TOKEN') if os.getenv('QASE_TOKEN') is not None else False
     IS_DOC_UPDATE_NEEDED = True if os.getenv('QASE_DOC') == 'True' else False
-
+    # TOKEN = os.getenv('QASE_TOKEN')
+    # IS_DOC_UPDATE_NEEDED = True
+    CASE_PARAMS = ['']
     API_DOMAIN = 'https://api.qase.io'
     FRONTEND_DOMAIN = 'https://app.qase.io'
     API_VERSION = 'v1'
