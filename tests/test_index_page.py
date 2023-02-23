ea8637ad751b49e6afc06e31ed0abd6ee629a7c8
Alexander Konukhov <jesusmisha@gmail.com>
2023-02-23 14:00:45 +0300

    fix function name

--- tests/test_index_page.py
+++ tests/test_index_page.py
@@ -5,7 +5,7 @@ import pages
 class TestFooter:
 
     @pytest.mark.case_id(1)  # 1 - test ID from Qase
-    def test_user_should_be_able_to_open_popup_select_subscription_plan(self, page):
+    def test_user_button_google_search_must_have_text_google_search(self, page):
         pages.index_page.open_index_page(page)
         pages.index_page.press_link_english_lang(page)
         actual_result = pages.index_page.get_text_from_google_search_button(page)
